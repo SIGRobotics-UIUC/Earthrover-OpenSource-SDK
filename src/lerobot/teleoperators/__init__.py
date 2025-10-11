5101f1caa2c78bca3d30e1d1ccfc273a2db7ab67
randomSmarts <67401115+randomSmarts@users.noreply.github.com>
2025-10-11 13:59:13 -0500

    deleted a message

--- src/lerobot/teleoperators/__init__.py
+++ src/lerobot/teleoperators/__init__.py
@@ -17,5 +17,3 @@
 from .config import TeleoperatorConfig
 from .teleoperator import Teleoperator
 from .utils import TeleopEvents, make_teleoperator_from_config
-
-# useless ahh file actual bs (all __init__.py)
