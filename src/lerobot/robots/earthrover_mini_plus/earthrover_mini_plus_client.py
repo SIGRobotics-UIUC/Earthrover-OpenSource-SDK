33439fe2148245cb10da3b6a5dfef7df97cfa7af
tyxin <h1810151@nushigh.edu.sg>
2025-10-18 14:45:55 -0500

    feat(update robot files)

---
+++ src/lerobot/robots/earthrover_mini_plus/earthrover_mini_plus_client.py
@@ -0,0 +1,68 @@
+import logging
+from socket import socket
+
+from lerobot.utils.errors import DeviceAlreadyConnectedError, DeviceNotConnectedError
+
+from ..robot import Robot
+from .config_earthrover_mini_plus import EarthRoverMiniPlusConfig
+
+
+class EarthRoverMiniPlusClient(Robot):
+    config_class = EarthRoverMiniPlusConfig
+    name = "earthrover_mini_plus_client"
+
+    def __init__(self, config: EarthRoverMiniPlusConfig):
+
+        super().__init__(config)
+        self.config = config
+
+        self.remote_ip = config.remote_ip
+        self.port_cmd = config.port_cmd
+        self.port_observations = config.port_observations
+
+        self.teleop_keys = config.teleop_keys
+        self.cameras = config.cameras
+
+        self.polling_timeout_ms = config.polling_timeout_ms
+        self.connect_timeout_s = config.connect_timeout_s
+
+        self.is_connected = False
+        self.logs = {}
+
+    @cached_property
+    def is_connected(self) -> bool:
+        return self.is_connected
+    
+    def connect(self) -> None:
+        """Establish ZMQ sockets with the remote mobile robot"""
+
+        if self.is_connected:
+            raise DeviceAlreadyConnectedError(
+                "Earthrover Mini Plus is already connected. Do not run `robot.connect()` twice."
+            )
+
+        self.socket = socket.create_connection((self.ip, self.port_cmd))
+        self.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+
+        # todo: check if this is valid
+        if self.socket.timeout:
+            raise DeviceNotConnectedError("Timeout waiting for Earthrover Mini Plus Host to connect expired.")
+
+        self._is_connected = True
+
+    def disconnect(self):
+
+        if not self._is_connected:
+            raise DeviceNotConnectedError(
+                "LeKiwi is not connected. You need to run `robot.connect()` before disconnecting."
+            )
+
+        if self.socket:
+            try:
+                self.socket.shutdown(socket.SHUT_RDWR)  # disabling send and receivedisables both send and receive
+            except OSError:
+                pass  # socket already closed or not connected
+            self.socket.close()
+            self.socket = None  # clear reference
+        
+        self._is_connected = False
--- src/lerobot/robots/earthrover_mini_plus/robot_earthrover_mini_plus.py
+++ src/lerobot/robots/earthrover_mini_plus/robot_earthrover_mini_plus.py
@@ -1,34 +1,13 @@
-#!/usr/bin/env python
-
-# Copyright 2024 The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import time
-from typing import Any      
-import warnings
 import logging
-
-
-
-import numpy as np
-from lerobot.cameras.utils import make_cameras_from_configs
-from lerobot.motors import Motor, MotorCalibration
+from socket import socket
 
 from lerobot.utils.errors import DeviceAlreadyConnectedError, DeviceNotConnectedError
 
+from lerobot.cameras.utils import make_cameras_from_configs
+
 from ..robot import Robot
 from .config_earthrover_mini_plus import EarthRoverMiniPlusConfig
+
 
 logger = logging.get_logger(__name__)
 
@@ -38,31 +17,60 @@ class EarthRoverMiniPlus(Robot):
     name = "earthrover_mini_plus"
 
     def __init__(self, config: EarthRoverMiniPlusConfig):
-        super().__init__(config)
 
+        super().__init__(config)
         self.config = config
 
+        self.base_motors = [] # todo
         self.cameras = make_cameras_from_configs(config.cameras)
 
-        self._last_command_time = time.time()
-        self._command_timeout = 1.0  # seconds
+    @cached_property
+    def is_connected(self) -> bool:
+        return self.is_connected
+    
+    def connect(self, calibrate: bool = True) -> None:
+        if self.is_connected:
+            raise DeviceAlreadyConnectedError(f"{self} already connected")
+        
+        self.configure()
 
-        self._state = {
-            "left_speed": 0.0,
-            "right_speed": 0.0,
-            # todo: anything else
-        }
+        if not self.is_calibrated and calibrate:
+            logging.info(
+                "Mismatch between calibration values in the motor and the calibration file or no calibration file found"
+            )
+            self.calibrate()
+            
+        logger.info(f"{self} connected")
+    
+    def is_calibrated(self) -> bool:
+        return self.is_calibrated
+    
+    def calibrate(self) -> None:
+        if self.calibration:
+            
+        logger.info(f"\nRunning calibration of {self}")
 
-        # any motors or sensors
-        # self.cameras = make_cameras_from_configs(config.cameras)
-
-        logger.info(f"{self.name} initialized.")
-
-    def configure(self) -> None:
-        # set up motors and sensors
-        logger.info(f"{self.name} configuring...")
-
-
+        # todo
+    
+    def configure(self):
+        # todo
+        pass
+    
     def get_observation(self) -> dict[str, Any]:
         if not self.is_connected:
-            raise DeviceNotConnectedError(f"{self} is not connected")
+            raise DeviceNotConnectedError(f"{self} is not connected.")
+
+    def send_action(self, action: dict[str, Any]) -> dict[str, Any]:
+        """Send control commands to Earthrover Mini Plus"""
+       
+        # send_ctl_cmd(self.socket, self.speed, self.angular)
+        
+        if not self.is_connected:
+            raise DeviceNotConnectedError(f"{self} is not connected.")
+
+
+    def disconnect(self):
+        if not self.is_connected:
+            raise DeviceNotConnectedError(f"{self} is not connected.")
+
+        logger.info(f"{self} disconnected.")
