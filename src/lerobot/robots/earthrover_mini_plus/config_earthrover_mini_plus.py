42dcf797c3327aff8c5727c43d15e981a387ffe2
tyxin <h1810151@nushigh.edu.sg>
2025-10-18 14:45:55 -0500

    feat(update robot files)

--- src/lerobot/robots/earthrover_mini_plus/config_earthrover_mini_plus.py
+++ src/lerobot/robots/earthrover_mini_plus/config_earthrover_mini_plus.py
@@ -43,4 +43,48 @@ class EarthRoverMiniPlusConfig(RobotConfig):
 
 
 # todo: maybe have client and host configs
+@dataclass
+class EarthRoverMiniPlusHostConfig:
+    # Network Configuration
+    port_cmd: int = 5555
+    port_observations: int = 5556
 
+    # Duration of the application
+    connection_time_s: int = 30
+
+    # Watchdog: stop the robot if no command is received for over 0.5 seconds.
+    watchdog_timeout_ms: int = 500
+
+    # If robot jitters decrease the frequency and monitor cpu load with `top` in cmd
+    max_loop_freq_hz: int = 30
+
+@RobotConfig.register_subclass("earthrover_mini_plus_client")  
+@dataclass
+class EarthRoverMiniPlusClientConfig:
+    # Network Configuration
+    remote_ip: str
+    port_cmd: int = 5555
+    port_observations: int = 5556
+
+    # todo: update this based on earthrover miniplus teleoperator
+    teleop_keys: dict[str, str] = field(
+        default_factory=lambda: {
+            # Movement
+            "forward": "w",
+            "backward": "s",
+            "left": "a",
+            "right": "d",
+            "rotate_left": "z",
+            "rotate_right": "x",
+            # Speed control
+            "speed_up": "r",
+            "speed_down": "f",
+            # quit teleop
+            "quit": "q",
+        }
+    )
+
+    cameras: dict[str, CameraConfig] = field(default_factory=earthrover_mini_plus_cameras_config)
+
+    polling_timeout_ms: int = 15
+    connect_timeout_s: int = 5
