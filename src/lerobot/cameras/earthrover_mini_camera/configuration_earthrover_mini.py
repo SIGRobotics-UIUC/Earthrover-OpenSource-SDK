f4da1bb5b9ede80c2658171992e68798d6cb15fc
haunters <hauntersgd@gmail.com>
2025-10-25 18:15:21 -0500

    updated test file and camera config/functions

--- src/lerobot/cameras/earthrover_mini_camera/configuration_earthrover_mini.py
+++ src/lerobot/cameras/earthrover_mini_camera/configuration_earthrover_mini.py
@@ -14,6 +14,7 @@
 
 from dataclasses import dataclass
 from pathlib import Path
+import warnings
 
 from ..configs import CameraConfig, ColorMode, Cv2Rotation
 
@@ -21,22 +22,19 @@ from ..configs import CameraConfig, ColorMode, Cv2Rotation
 @CameraConfig.register_subclass("earthrover_mini_camera")
 @dataclass
 class EarthRoverMiniCameraConfig(CameraConfig):
-    """Configuration class for OpenCV-based camera devices or video files.
+    """Configuration class for EarthRoverMini camera devices.
 
     This class provides configuration options for cameras accessed through OpenCV,
     supporting both physical camera devices and video files. It includes settings
     for resolution, frame rate, color mode, and image rotation.
 
-    Example configurations:
+    Example configuration:
     ```python
-    # Basic configurations
-    OpenCVCameraConfig(0, 30, 1280, 720)   # 1280x720 @ 30FPS
-    OpenCVCameraConfig(/dev/video4, 60, 640, 480)   # 640x480 @ 60FPS
-
-    # Advanced configurations
-    OpenCVCameraConfig(128422271347, 30, 640, 480, rotation=Cv2Rotation.ROTATE_90)     # With 90° rotation
+    EarthRoverMiniCameraConfig(
+            index_or_path=EarthRoverMiniCameraConfig.FRONT_CAM_MAIN,  # front main stream
+            color_mode=ColorMode.RGB
+    )
     ```
-
     Attributes:
         index_or_path: Either an integer representing the camera device index,
                       or a Path object pointing to a video file.
@@ -56,18 +54,45 @@ class EarthRoverMiniCameraConfig(CameraConfig):
     REAR_CAM_MAIN: str = "rtsp://192.168.11.1/live/2"
     REAR_CAM_SUB: str = "rtsp://192.168.11.1/live/3"
 
+    # Does not change actual fps, width, and height, just for camera info/logging purposes
+    DEFAULT_FPS: float = 30.0 
+
+    MAIN_WIDTH: int = 1920
+    MAIN_HEIGHT: int = 1080
+
+    SUB_WIDTH: int = 720
+    SUB_HEIGHT: int = 576
+
     index_or_path: str = FRONT_CAM_MAIN
+    fps: float = DEFAULT_FPS
     color_mode: ColorMode = ColorMode.RGB
     rotation: Cv2Rotation = Cv2Rotation.NO_ROTATION
     warmup_s: int = 1
 
     def __post_init__(self):
 
-        allowed_cams = {self.FRONT_CAM_MAIN, self.FRONT_CAM_SUB, self.REAR_CAM_MAIN, self.REAR_CAM_SUB}
-        if self.index_or_path not in allowed_cams:
+        main_cams = {self.FRONT_CAM_MAIN, self.REAR_CAM_MAIN}
+        sub_cams = {self.FRONT_CAM_SUB, self.REAR_CAM_SUB}
+        if self.index_or_path not in main_cams and self.index_or_path not in sub_cams:
             raise ValueError(
-                f"index_or_path must be one of four allowed camera URLS: {allowed_cams}"
+                f"index_or_path must be one of four allowed camera URLS: {main_cams} {sub_cams}"
             )
+        
+        if self.fps is not None and self.fps != self.DEFAULT_FPS:
+            warnings.warn("FPS cannot be modified for this camera — using default (30).")
+            self.fps = self.DEFAULT_FPS
+        
+        if self.index_or_path in main_cams:
+            if (self.width is not None and self.width != self.MAIN_WIDTH) or (self.height is not None and self.height != self.MAIN_HEIGHT):
+                warnings.warn("Resolution cannot be modified for this main camera — using default (1920x1080).")
+            self.width = self.MAIN_WIDTH
+            self.height = self.MAIN_HEIGHT
+        
+        if self.index_or_path in sub_cams:
+            if (self.width is not None and self.width != self.SUB_WIDTH) or (self.height is not None and self.height != self.SUB_HEIGHT):
+                warnings.warn("Resolution cannot be modified for this sub camera — using default (720x576).")
+            self.width = self.SUB_WIDTH
+            self.height = self.SUB_HEIGHT
 
         if self.color_mode not in (ColorMode.RGB, ColorMode.BGR):
             raise ValueError(
