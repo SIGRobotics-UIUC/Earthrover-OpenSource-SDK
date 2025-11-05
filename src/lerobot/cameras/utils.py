fcea435c756cc20e3e0ae433f0712681da202849
Saketh Kantipudi <saketh.kantipudi@gmail.com>
2025-11-05 01:15:34 -0600

    test version semi-working

--- src/lerobot/cameras/utils.py
+++ src/lerobot/cameras/utils.py
@@ -28,6 +28,7 @@ def make_cameras_from_configs(camera_configs: dict[str, CameraConfig]) -> dict[s
     cameras = {}
 
     for key, cfg in camera_configs.items():
+        print(f"[KEY {key}] CONFIG = {cfg}")
         if cfg.type == "opencv":
             from .opencv import OpenCVCamera
 
